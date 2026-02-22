6332813df8642bb1a051212cfca179f376d97215
freska2234 <74221741+freska2234@users.noreply.github.com>
2026-02-22 15:50:36 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,8 +15,8 @@ from telegram.ext import Application, CommandHandler, MessageHandler, CallbackQu
 BOT_TOKEN  = "8334507568:AAEJakB6G_kPVNOX6r3vZGc8ZqcLPhOmCLM"
 ADMIN_IDS  = [5895491379, 6220135474, 844663875]
 
-LOGIN_EMAIL    = "owyxtc64686m+zpup@outlook.com"
-LOGIN_PASSWORD = "owyxtc64686m+zpup@outlook.com"
+LOGIN_EMAIL    = "budrigerto@necub.com"
+LOGIN_PASSWORD = "111222333Mm"
 
 # ========== AUTO LOGIN ==========
 dobies_session = None
